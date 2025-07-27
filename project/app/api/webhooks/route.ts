47c7734575c18a9a3535825dda0cac8465199fe1
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-07-27 19:08:53 +0800

    refactor(auth): add conditionals with logic placeholders in webhooks
    Add conditionals to handle event types of user.created, user.deleted and
    user.updated with placeholder logic. Placeholder logic to be replaced
    when database is implemented.
    
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/app/api/webhooks/route.ts
+++ project/app/api/webhooks/route.ts
@@ -1,20 +1,30 @@
-import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { NextRequest } from 'next/server'
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const event = await verifyWebhook(req)
+    const event = await verifyWebhook(req);
+    const { id } = event.data;
+    const eventType = event.type;
+    // ------ Log ------ //
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`,
+    );
+    console.log("Webhook payload:", event.data);
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = event.data
-    const eventType = event.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', event.data)
+    // ------ Log ------ //
 
-    return new Response('Webhook received', { status: 200 })
+    if (eventType == "user.created") {
+      // Logic for inserting the user fields here for the db.
+    } else if (eventType == "user.updated") {
+      // Logic for updating the user fields here for the db.
+    } else if (eventType == "user.deleted") {
+      // Logic for archiving the user here for the db.
+    }
+
+    return new Response("Webhook received", { status: 200 });
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error verifying webhook', { status: 400 })
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
   }
 }
