333d53575547f68d3f47a121146f66108a943208
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-08-04 08:00:44 +0800

    fix(db): use drizzle pulled relations in db index to be able to query
    - Used the relations.ts added by drizzle pull to the config of drizzle
      in db-index.ts so that drizzle is able to recognize relations between
      tables.
    - Remove old relations.ts.
    - Also add schema.ts file as a byproduct of drizzle pull, but is
      currently not being used for anything, since across the app, the
      schema.ts in lib/db is still being used.
    
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

---
+++ project/drizzle/relations.ts
@@ -0,0 +1,74 @@
+import { relations } from "drizzle-orm/relations";
+import { tasks, comments, projects, lists, users, teams, teamsToProjects, usersToTasks, usersToTeams } from "./schema";
+
+export const commentsRelations = relations(comments, ({one}) => ({
+	task: one(tasks, {
+		fields: [comments.taskId],
+		references: [tasks.id]
+	}),
+}));
+
+export const tasksRelations = relations(tasks, ({many}) => ({
+	comments: many(comments),
+	usersToTasks: many(usersToTasks),
+}));
+
+export const listsRelations = relations(lists, ({one}) => ({
+	project: one(projects, {
+		fields: [lists.projectId],
+		references: [projects.id]
+	}),
+}));
+
+export const projectsRelations = relations(projects, ({one, many}) => ({
+	lists: many(lists),
+	user: one(users, {
+		fields: [projects.ownerId],
+		references: [users.id]
+	}),
+	teamsToProjects: many(teamsToProjects),
+}));
+
+export const usersRelations = relations(users, ({many}) => ({
+	projects: many(projects),
+	usersToTasks: many(usersToTasks),
+	usersToTeams: many(usersToTeams),
+}));
+
+export const teamsToProjectsRelations = relations(teamsToProjects, ({one}) => ({
+	team: one(teams, {
+		fields: [teamsToProjects.teamId],
+		references: [teams.id]
+	}),
+	project: one(projects, {
+		fields: [teamsToProjects.projectId],
+		references: [projects.id]
+	}),
+}));
+
+export const teamsRelations = relations(teams, ({many}) => ({
+	teamsToProjects: many(teamsToProjects),
+	usersToTeams: many(usersToTeams),
+}));
+
+export const usersToTasksRelations = relations(usersToTasks, ({one}) => ({
+	task: one(tasks, {
+		fields: [usersToTasks.taskId],
+		references: [tasks.id]
+	}),
+	user: one(users, {
+		fields: [usersToTasks.userId],
+		references: [users.id]
+	}),
+}));
+
+export const usersToTeamsRelations = relations(usersToTeams, ({one}) => ({
+	team: one(teams, {
+		fields: [usersToTeams.teamId],
+		references: [teams.id]
+	}),
+	user: one(users, {
+		fields: [usersToTeams.userId],
+		references: [users.id]
+	}),
+}));
