03c934f7fa8ff19e8032a517e27fe889d1bc0165
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-08-19 13:03:31 +0800

    chore: add loading file for projects slug
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/app/(dashboard)/projects/loading.tsx
+++ project/app/(dashboard)/projects/loading.tsx
@@ -1,3 +1,8 @@
+import { Loader2Icon } from "lucide-react";
+
 export default function Loading() {
-  return null
+  <div className="w-full h-full flex justify-center items-center gap-x-2">
+    {" "}
+    <Loader2Icon size={24} className="animate-spin"></Loader2Icon> <p className="text-2xl"> Loading </p>
+  </div>;
 }
