d2546ab04af97c19e9045ba7f5ce10d8d38a81e1
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-08-19 10:47:47 +0800

    feat(tasks): revamp task card
    - Include drag handle
    - Add Due date display
    - Add comments and labels placeholders
    
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/components/tasks/task-card.tsx
+++ project/components/tasks/task-card.tsx
@@ -9,6 +9,11 @@ import { capitalize, taskPriorityColor } from "@/lib/utils";
 import { useTasks } from "@/hooks/use-tasks";
 import TaskOptions from "./task-options";
 import UpdateTaskModal from "../modals/update-task-modal";
+import { Calendar, GripVertical, MessageCircleMore } from "lucide-react";
+
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { formatDate } from "../../lib/utils";
 
 /*
 TODO: Implementation Notes for Interns:
@@ -68,33 +73,72 @@ const TaskCard: FC<TaskCardProps> = ({ task, list_id, project_id }) => {
           setIsModalOpen={setEditModalOpen}
         />
       )}
-      <div className="group mx-4 p-4 bg-white dark:bg-transparent  shadow-md rounded-lg border border-border cursor-pointer hover:shadow-lg transition-shadow">
-        <div className="flex justify-between">
-          <h4 className="font-medium text-foreground/85 text-sm mb-2">{task.title}</h4>
+      <Card className="group mx-4 p-0 gap-0 ">
+        <CardHeader className="px-1 py-2 justify-between items-center flex flex-row border-b-2 border-secondary relative">
+          {/* Drag Button, Options Button */}
+          <button className="p-1 text-secondary-foreground/50 hover:bg-foreground/10 hover:text-foreground active:bg-foreground/10 active:text-foreground rounded-md cursor-grab transition-all">
+            <GripVertical />
+          </button>
           <TaskOptions
             task_id={task.id}
             list_id={list_id}
             setEditModalOpen={() => setEditModalOpen(true)}
             className="opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300"
           />
-        </div>
+        </CardHeader>
+        <CardContent className="p-3">
+          <div className="flex flex-col gap-2">
+            {/* Title, Priority, Description */}
+            <div className="flex justify-between">
+              <p className="font-medium text-foreground/85 text-sm">{task.title}</p>
+              <Badge className={`${taskPriorityColor[task.priority]}`}>{capitalize(task.priority)}</Badge>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-xs text-foreground/65">{task.description}</p>
+            </div>
 
-        <p className="text-xs text-foreground/65 mb-3">{task.description}</p>
-        <div className="flex items-center justify-between">
-          <Badge className={`${taskPriorityColor[task.priority]}`}>{capitalize(task.priority)}</Badge>
-          {isTaskMembersLoading ? (
-            <Skeleton height="5" width="24" />
-          ) : taskMembers && !getTaskMembersError ? (
-            taskMembers.length === 0 ? (
-              <p className="text-xs text-foreground/65">None Assigned</p>
-            ) : (
-              <MembersAvatars members={taskMembers} max_visible={5} size={5} />
-            )
-          ) : (
-            <p className="text-xs text-foreground/65">Unable to load members.</p>
-          )}
-        </div>
-      </div>
+            {/* Comments Count and Due Date */}
+            <div className="flex justify-between items-center mt-2">
+              <div className="flex justify-between gap-4">
+                <div className="inline-flex gap-1">
+                  <MessageCircleMore size={14} className="text-foreground/65" />
+                  <p className="font-base text-muted-foreground text-xs  "> 14</p>
+                </div>
+                <div className="inline-flex gap-1">
+                  <Calendar size={14} className="text-foreground/65" />
+                  <p className="font-base text-muted-foreground text-xs ">
+                    {" "}
+                    {task.dueDate ? `${formatDate(task.dueDate)}` : "No due date"}{" "}
+                  </p>
+                </div>
+              </div>
+
+              {/* Assignee Members */}
+              <div className="flex items-center justify-end">
+                {isTaskMembersLoading ? (
+                  <Skeleton height="5" width="24" />
+                ) : taskMembers && !getTaskMembersError ? (
+                  taskMembers.length === 0 ? (
+                    <p className="text-xs text-foreground/65">None Assigned</p>
+                  ) : (
+                    <MembersAvatars members={taskMembers} max_visible={5} size={5} />
+                  )
+                ) : (
+                  <p className="text-xs text-foreground/65">Unable to load members.</p>
+                )}
+              </div>
+            </div>
+            {/* Placeholder Labels */}
+            <div className="flex gap-1.5">
+              {["Late", "Frontend", "Documentation"].map((l, idx) => (
+                <Badge key={idx} className="text-[9px]">
+                  {l}
+                </Badge>
+              ))}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </>
   );
 };
