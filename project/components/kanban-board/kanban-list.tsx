2ecbd3c5dbaa170026652090fa1749032b7f0207
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-08-19 12:28:06 +0800

    chore(list): Add drag button to kanban-list
    - Add Kanban list drag handle.
    - Wrap tasks in a DND context.
    
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/components/kanban-board/kanban-list.tsx
+++ project/components/kanban-board/kanban-list.tsx
@@ -6,6 +6,10 @@ import { useTasks } from "@/hooks/use-tasks";
 import CreateTaskModal from "../modals/create-task-modal";
 import { Loader2Icon } from "lucide-react";
 import TaskCard from "../tasks/task-card";
+import { DndContext } from "@dnd-kit/core";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { DragButton } from "../ui/drag-button";
 
 type KanbanListProps = {
   list: ListSelect;
@@ -28,6 +32,19 @@ const KanbanList: FC<KanbanListProps> = ({ list, project_id, onEdit, searchTerm 
     setCreateModalOpen(true);
   }
 
+  const { setNodeRef, listeners, attributes, transform, transition } = useSortable({
+    id: list.id,
+    data: {
+      type: "list",
+      list,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   return (
     <>
       {isCreateModalOpen && listTasks && (
@@ -39,26 +56,33 @@ const KanbanList: FC<KanbanListProps> = ({ list, project_id, onEdit, searchTerm 
           position={listTasks.length}
         />
       )}
-      <div className="min-w-[80px] min-h-[350px] w-80 overflow-y shrink-0">
+      <div ref={setNodeRef} style={style} className="min-w-[80px] min-h-[350px] w-80 overflow-y shrink-0">
         <div className="bg-primary/7 dark:bg-dark-grey-900 min-h-[350px] h-full rounded-lg border border-border ">
           <div className="p-4 border-b border-border">
             <div className="flex items-center justify-between">
-              <p className="font-semibold text-foreground text-sm">
-                {list.name}
-                <span className="ml-2 px-2 py-1 text-xs bg-foreground/10 rounded-full">{list.position}</span>
-              </p>
+              <div className="flex gap-3 items-center">
+                <DragButton listeners={listeners} attributes={attributes} />
+                <p className="font-semibold text-foreground text-sm">
+                  {list.name}
+                  <span className="ml-2 px-2 py-1 text-xs bg-foreground/10 rounded-full">{list.position}</span>
+                </p>
+              </div>
+
               <KanbanListOptions project_id={project_id} list_id={list.id} onEdit={onEdit} />
             </div>
           </div>
 
+          {/* Scrollable Task Content */}
           {filteredTasks ? (
-            <div className="min-h-[400px]">
-              <div className="scrollbar-custom flex flex-col py-4 mb-4 space-y-3 min-h-[400px] max-h-[400px] overflow-y-auto">
-                {filteredTasks.map((task) => (
-                  <TaskCard key={task.id} task={task} list_id={list.id} project_id={project_id} />
-                ))}
+            <DndContext>
+              <div className="min-h-[400px]">
+                <div className="scrollbar-custom flex flex-col py-4 mb-4 space-y-3 min-h-[400px] max-h-[400px] overflow-y-auto">
+                  {filteredTasks.map((task) => (
+                    <TaskCard key={task.id} task={task} list_id={list.id} project_id={project_id} />
+                  ))}
+                </div>
               </div>
-            </div>
+            </DndContext>
           ) : isListTasksLoading && !getListTasksError ? (
             <div className="p-4 text-center w-full h-full flex justify-center items-center gap-x-2">
               {" "}
